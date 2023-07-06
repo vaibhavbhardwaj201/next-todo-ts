77057694db7f4f3fb45a6e9720f6c033fd40f64b
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-07-06 19:27:54 +0300

    ADD active class to navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
+"use client"
+
 import Link from 'next/link'
+import { useSearchParams } from 'next/navigation'
 import React from 'react'
 
 const Navbar = () => {
+  const searchParams = useSearchParams()
+  const params = searchParams.get('todos')
+
   return (
     <div>
-        <Link href={'/'}>All</Link>
-        <Link href={'/?todos=active'}>Active</Link>
-        <Link href={'/?todos=completed'}>Completed</Link>
+        <Link href={'/'} className={params === null ? 'active' : ''}>All</Link>
+        <Link href={'/?todos=active'} className={params === 'active' ? 'active' : ''}>Active</Link>
+        <Link href={'/?todos=completed'} className={params === 'completed' ? 'active' : ''}>Completed</Link>
     </div>
   )
 }
